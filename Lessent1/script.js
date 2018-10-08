23fde2f216e0bd483b9d66592f0527fbc0ca0458
Aliya <tamiris0303@mail.ru>
2018-10-08 16:05:58 +0600

    четвертый комит

--- Lessent1/script.js
+++ Lessent1/script.js
@@ -1,29 +1,23 @@
-
 let money=prompt("Ваш бюджет на месяц?");
     time=prompt("date:дата формирования");
     money1=+money;
     console.log(money);
     console.log(time);
 let appData={
-    butzhet:"money",
+    Budget:"money",
     timeData:"time",
+    income:"",
+    savings:false,
+    optionalExpenses:"",
     };
-let optionalExpenses={
-    };
-let  expenses={ 
-};
-
- let income=[
-
- ];
- savings=false;
-let article1=prompt("Введите обязательную статью расходов в этом месяце");
+    let article1=prompt("Введите обязательную статью расходов в этом месяце");
 let price1=prompt("Во сколько обойдется?");
 let article2=prompt("Введите обязательную статью расходов в этом месяце");
 let price2=prompt("Во сколько обойдется?");
-expenses["vopros1"]=article1;
-expenses["vopros2"]=price1;
-console.log(expenses.vopros1);
-console.log(expenses.vopros2);
+let  expenses={ question1:"article1",
+    question2:"price1",
+};
 let day=30;
 alert(money/day);
+console.log(expenses.question1.article1);
+console.log(expenses.question2.price1);
